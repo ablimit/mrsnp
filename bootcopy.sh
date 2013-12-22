2f9de444a9de1c06119dd62341baa647436efe6c
Ablimit Aji <ablimit@gmail.com>
2013-12-22 17:58:27 -0500

    bugfix

--- bootcopy.sh
+++ bootcopy.sh
@@ -7,4 +7,5 @@ hadoop fs -get s3://aaji/scratch/s3cfg .s3cfg
 # to copy jar 
 s3cmd get s3://aaji/scratch/snpcode/snp.jar snp.jar
 
+# hadoop jar snp.jar Vcftoplink s3://aaji/data/snp s3://aaji/scratch/snpout 1 93 0.001 93834
 
